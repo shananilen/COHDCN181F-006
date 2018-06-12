b717531f268b39a69a593c2982eedcde01494100
Shanan Ilen <39610635+shananilen@users.noreply.github.com>
2018-06-12 18:27:35 +0530

    Delete 2.py

--- 2.py
+++
@@ -1,6 +0,0 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
-s.setsockopt(socket.SOL_SOCKET, socket.REUSEADDR, 1)
-s.setsockopt(socket.SOL_SOCKET, socket.IP_HDRINCL)
-s.sendto('127.0.0.1'(80,0))
