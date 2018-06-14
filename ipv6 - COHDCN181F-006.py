1635a70badb4a6323bb227b443f32a7410495c36
Shanan Ilen <39610635+shananilen@users.noreply.github.com>
2018-06-14 23:35:44 +0530

    Update ipv6 - COHDCN181F-006.py

--- ipv6 - COHDCN181F-006.py
+++ ipv6 - COHDCN181F-006.py
@@ -2,7 +2,7 @@ import struct
 from ctypes import *
 
 class IPv6(structure):
-    _fileds_ = {
+    _fileds_ = [
         ("version" , c_ubyte, 6),
         ("class", c_ubyte),
         ("lable", c_ubyte , 20),
@@ -11,7 +11,7 @@ class IPv6(structure):
         ("hoplimit", c_ubyte),
         ("src", c_unit128),
         ("dst", c_unit128)
-        }
+    ]
 
 def __new__ (self, socket_buffer = None):
     return self.from_buffer_copy(socket_buffer)
